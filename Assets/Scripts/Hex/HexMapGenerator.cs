d7b3ad83c660038ee6fafd49688b81ac345efa26
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-12-28 20:46:38 +0800

    地形的分块

--- Assets/Scripts/Hex/HexMapGenerator.cs
+++ Assets/Scripts/Hex/HexMapGenerator.cs
@@ -8,6 +8,15 @@ using UnityEngine;
 public class HexMapGenerator : MonoBehaviour
 {
     /// <summary>
+    /// 陆地生成的XZ的边界
+    /// </summary>
+    private struct MapRegion
+    {
+        public int xMin, xMax, zMin, zMax;
+    }
+
+
+    /// <summary>
     /// 单例化用
     /// </summary>
     public static HexMapGenerator Instance { get; private set; }
@@ -28,16 +37,34 @@ public class HexMapGenerator : MonoBehaviour
     [Range(1, 5)] public int waterLevel = 3;
 
     /// <summary>
+    /// 陆地生成的X边界
+    /// </summary>
+    [Range(0, 10)] public int mapBorderX = 5;
+
+    /// <summary>
+    /// 陆地生成的Z边界
+    /// </summary>
+    [Range(0, 10)] public int mapBorderZ = 5;
+
+    /// <summary>
+    /// 每小个地形块的边界
+    /// </summary>
+    [Range(0, 0)] public int regionBorder = 5;
+
+    /// <summary>
+    /// 最多生成几个区域块
+    /// </summary>
+    [Range(1, 4)] public int regionCount = 1;
+
+    /// <summary>
     /// 最低的的高度
     /// </summary>
-    [Range(-4, 0)]
-    public int elevationMinimum = -2;
+    [Range(-4, 0)] public int elevationMinimum = -2;
 
     /// <summary>
     /// 最高的高度
     /// </summary>
-    [Range(6, 10)]
-    public int elevationMaximum = 8;
+    [Range(6, 10)] public int elevationMaximum = 8;
 
     /// <summary>
     /// 生成孤立悬崖的概率,即高度+1/+2
@@ -47,8 +74,7 @@ public class HexMapGenerator : MonoBehaviour
     /// <summary>
     /// 地形下降的概率
     /// </summary>
-    [Range(0, 0.4f)]
-    public float sinkProbability = 0.2f;
+    [Range(0, 0.4f)] public float sinkProbability = 0.2f;
 
     /// <summary>
     /// 随机生成地形的圆形程度,越小越圆
@@ -66,13 +92,14 @@ public class HexMapGenerator : MonoBehaviour
     [Range(20, 200)] public int chunkSizeMax = 100;
 
     /// <summary>
-    /// 水的百分比
+    /// 陆地的块的百分比
     /// </summary>
     [Range(0.05f, 0.95f)] public float landPercentage = 0.5f;
 
-    private int cellCount;//一共有几个细胞
-    private HexCellPriorityQueue searchFrontier;//随机生成寻路队列
-    private int searchFrontierPhase;//随机生成寻路值
+    private int cellCount; //一共有几个细胞
+    private HexCellPriorityQueue searchFrontier; //随机生成寻路队列
+    private int searchFrontierPhase; //随机生成寻路值
+    private List<MapRegion> regions; //陆地生成的XZ的边界
 
     private void Awake()
     {
@@ -89,10 +116,11 @@ public class HexMapGenerator : MonoBehaviour
         if (!useFixedSeed)
         {
             seed = Random.Range(0, int.MaxValue);
-            seed ^= (int)System.DateTime.Now.Ticks;
-            seed ^= (int)Time.unscaledTime;
-            seed &= int.MaxValue;//强制归正数
+            seed ^= (int) System.DateTime.Now.Ticks;
+            seed ^= (int) Time.unscaledTime;
+            seed &= int.MaxValue; //强制归正数
         }
+
         Random.InitState(seed);
 
         //-----生成基础地形
@@ -112,10 +140,13 @@ public class HexMapGenerator : MonoBehaviour
             HexGrid.Instance.GetCell(i).WaterLevel = waterLevel;
         }
 
-        //
+        //陆地生成的边界
+        CreateRegions();
+
+        //创建岛屿
         CreateLand();
 
-        //
+        //创建地图类型
         SetTerrainType();
 
         //重置寻路值
@@ -129,16 +160,112 @@ public class HexMapGenerator : MonoBehaviour
     }
 
     /// <summary>
+    /// 陆地生成的边界
+    /// </summary>
+    private void CreateRegions()
+    {
+        if (regions == null)
+        {
+            regions = new List<MapRegion>();
+        }
+        else
+        {
+            regions.Clear();
+        }
+
+        var grid = HexGrid.Instance;
+        MapRegion region;
+        int countX = grid.cellCountX, countZ = grid.cellCountZ;
+        switch (regionCount)
+        {
+            //1或者其他
+            default:
+            {
+                region.xMin = mapBorderX;
+                region.xMax = countX - mapBorderX;
+                region.zMin = mapBorderZ;
+                region.zMax = countZ - mapBorderZ;
+                regions.Add(region);
+                break;
+            }
+            case 2:
+            {
+                if (Random.value < 0.5f)
+                {
+                    var halfX = countX / 2;
+                    region.xMin = mapBorderX;
+                    region.xMax = halfX - regionBorder;
+                    region.zMin = mapBorderZ;
+                    region.zMax = countZ - mapBorderZ;
+                    regions.Add(region);
+                    region.xMin = halfX + regionBorder;
+                    region.xMax = countX - mapBorderX;
+                    regions.Add(region);
+                }
+                else
+                {
+                    var halfZ = countZ / 2;
+                    region.xMin = mapBorderX;
+                    region.xMax = countX - mapBorderX;
+                    region.zMin = mapBorderZ;
+                    region.zMax = halfZ - regionBorder;
+                    regions.Add(region);
+                    region.zMin = halfZ + regionBorder;
+                    region.zMax = countZ - mapBorderZ;
+                    regions.Add(region);
+                }
+
+                break;
+            }
+            case 3:
+            {
+                int x_1_3 = countX / 3, x_2_3 = x_1_3 * 2;
+                region.xMin = mapBorderX;
+                region.xMax = x_1_3 - regionBorder;
+                region.zMin = mapBorderZ;
+                region.zMax = countZ - mapBorderZ;
+                regions.Add(region);
+                region.xMin = x_1_3 + regionBorder;
+                region.xMax = x_2_3 - regionBorder;
+                regions.Add(region);
+                region.xMin = x_2_3 + regionBorder;
+                region.xMax = countX - mapBorderX;
+                regions.Add(region);
+                break;
+            }
+            case 4:
+            {
+                int halfX = countX / 2, halfZ = countZ / 2;
+                region.xMin = mapBorderX;
+                region.xMax = halfX - regionBorder;
+                region.zMin = mapBorderZ;
+                region.zMax = halfZ - regionBorder;
+                regions.Add(region);
+                region.xMin = halfX + regionBorder;
+                region.xMax = countX - mapBorderX;
+                regions.Add(region);
+                region.zMin = halfZ + regionBorder;
+                region.zMax = countZ - mapBorderZ;
+                regions.Add(region);
+                region.xMin = mapBorderX;
+                region.xMax = countX - regionBorder;
+                regions.Add(region);
+                break;
+            }
+        }
+    }
+
+    /// <summary>
     /// 上升地形
     /// </summary>
     /// <param name="chunkSize">要上升的块</param>
     /// <param name="budget">水的块数</param>
     /// <returns>还剩下多少水的块</returns>
-    private int RaiseTerrain(int chunkSize, int budget)
+    private int RaiseTerrain(int chunkSize, int budget, MapRegion region)
     {
         //-----随机得到一个细胞
         searchFrontierPhase += 1;
-        HexCell firstCell = GetRandomCell();
+        HexCell firstCell = GetRandomCell(region);
         firstCell.SearchPhase = searchFrontierPhase;
         firstCell.Distance = 0;
         firstCell.SearchHeuristic = 0;
@@ -147,7 +274,7 @@ public class HexMapGenerator : MonoBehaviour
 
         //这次要上升几个高度
         int rise = Random.value < highRiseProbability ? 2 : 1;
-        int size = 0;//已经上升的块
+        int size = 0; //已经上升的块
         //上升的块<要上升的块&&寻路队列里还有cell
         while (size < chunkSize && searchFrontier.Count > 0)
         {
@@ -156,9 +283,11 @@ public class HexMapGenerator : MonoBehaviour
             int originalElevation = current.Elevation;
             int newElevation = originalElevation + rise;
             if (newElevation > elevationMaximum)
-            {//如果新高度超过最高高度,就放弃
+            {
+                //如果新高度超过最高高度,就放弃
                 continue;
             }
+
             current.Elevation = newElevation;
 
             //如果改变的这块高度出水了,则水的块-1
@@ -197,10 +326,10 @@ public class HexMapGenerator : MonoBehaviour
     /// <param name="chunkSize">要下降的块</param>
     /// <param name="budget">水的块数</param>
     /// <returns>还剩下多少水的块</returns>
-    private int SinkTerrain(int chunkSize, int budget)
+    private int SinkTerrain(int chunkSize, int budget, MapRegion region)
     {
         searchFrontierPhase += 1;
-        HexCell firstCell = GetRandomCell();
+        HexCell firstCell = GetRandomCell(region);
         firstCell.SearchPhase = searchFrontierPhase;
         firstCell.Distance = 0;
         firstCell.SearchHeuristic = 0;
@@ -249,10 +378,13 @@ public class HexMapGenerator : MonoBehaviour
     /// 得到一个随机的cell
     /// </summary>
     /// <returns></returns>
-    private HexCell GetRandomCell()
+    private HexCell GetRandomCell(MapRegion region)
     {
         var grid = HexGrid.Instance;
-        return grid.GetCell(Random.Range(0, cellCount));
+
+        return grid.GetCell(
+            Random.Range(region.xMin, region.xMax)
+            , Random.Range(region.zMin, region.zMax));
     }
 
     /// <summary>
@@ -261,15 +393,33 @@ public class HexMapGenerator : MonoBehaviour
     private void CreateLand()
     {
         int landBudget = Mathf.RoundToInt(cellCount * landPercentage);
-        while (landBudget > 0)
+        for (int guard = 0; landBudget > 0 && guard < 1000; guard++)
         {
-            //这个块有几个cell
-            int chunkSize = Random.Range(chunkSizeMin, chunkSizeMax - 1);
+            bool isSink = Random.value < sinkProbability;
+            foreach (var region in regions)
+            {
+                //这个块有几个cell
+                int chunkSize = Random.Range(chunkSizeMin, chunkSizeMax - 1);
 
-            //根据概率是否要上升下降块,并且得到剩下的水的块
-            landBudget = Random.value < sinkProbability 
-                ? SinkTerrain(chunkSize, landBudget) 
-                : RaiseTerrain(chunkSize, landBudget);
+                //根据概率是否要上升下降块,并且得到剩下的水的块
+                if (isSink)
+                {
+                    landBudget = SinkTerrain(chunkSize, landBudget, region);
+                }
+                else
+                {
+                    landBudget = RaiseTerrain(chunkSize, landBudget, region);
+                    if (landBudget == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (landBudget > 0)
+        {
+            Debug.Log("无法生成这么多的陆地的块,还剩下" + landBudget.ToString());
         }
     }
 
